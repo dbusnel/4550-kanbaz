5825b73213063a3462631755f77696201ae5b9b1
dbusnel <dbusnel@icloud.com>
2026-04-06 16:50:52 -0400

    updates to pazza

--- app/(kambaz)/courses/[cid]/pazza/[pid]/page.tsx
+++ app/(kambaz)/courses/[cid]/pazza/[pid]/page.tsx
@@ -4,6 +4,12 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../../../store";
 import * as client from "../../../client";
 import { useEffect, useState } from "react";
+import { TbSquareLetterSFilled, TbSquareLetterIFilled } from "react-icons/tb";
+import { FormControl, FormSelect } from "react-bootstrap";
+
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+import { FaUserCircle } from "react-icons/fa";
 
 interface userInfo {
   firstName: string;
@@ -17,6 +23,8 @@ export default function PazzaPost() {
   const { currentUser } = useSelector(
     (state: RootState) => state.accountReducer,
   );
+
+  const [value, setValue] = useState("");
 
   const [displayPost, setDisplayPost] = useState();
 
@@ -65,6 +73,13 @@ export default function PazzaPost() {
     ? new Date(displayPost.updatedAt).toLocaleString()
     : "N/A";
 
+  const studentAnswer = displayPost.studentAnswers[0]
+    ? displayPost.studentAnswers[0]
+    : undefined;
+  const instructorAnswer = displayPost.instructorAnswers[0]
+    ? displayPost.instructorAnswers[0]
+    : undefined;
+
   return (
     <div>
       <h1 style={{ fontWeight: "bold" }}>{displayPost.summary}</h1>
@@ -93,6 +108,90 @@ export default function PazzaPost() {
         {viewCount} view{viewCount !== 1 ? "s" : ""}
       </div>
       <hr />
+      {displayPost.type === "question" && (
+        <div>
+          <span className="flex flex-row">
+            <TbSquareLetterSFilled
+              className="w-10"
+              style={{ fontSize: "32px" }}
+            />
+            <h3>Student answer</h3>
+          </span>
+          {studentAnswer !== undefined ? (
+            <div>
+              <p>
+                Last updated at{" "}
+                {new Date(studentAnswer.updatedAt).toLocaleString()}
+              </p>
+              <p>{studentAnswer.content}</p>
+            </div>
+          ) : (
+            <p>Enter text here</p>
+          )}
+          <div className="w-25">
+            <FormSelect className="w-4">
+              <option value="actions" defaultChecked>
+                Actions
+              </option>
+              <option value="edit"> Edit </option>
+              <option value="delete"> Delete </option>
+            </FormSelect>
+          </div>
+          <hr />
+          <span className="flex flex-row">
+            <TbSquareLetterIFilled
+              className="w-10"
+              style={{ fontSize: "32px" }}
+            />
+            <h3>Instructor answer</h3>
+          </span>
+          {instructorAnswer ? (
+            <div>
+              <p>
+                Last updated at{" "}
+                {new Date(instructorAnswer.updatedAt).toLocaleString()}
+              </p>
+              <p>{instructorAnswer.content}</p>
+            </div>
+          ) : (
+            <p>Enter text here</p>
+          )}
+          <div className="w-25">
+            <FormSelect className="w-4">
+              <option value="actions" defaultChecked>
+                Actions
+              </option>
+              <option value="edit"> Edit </option>
+              <option value="delete"> Delete </option>
+            </FormSelect>
+          </div>
+          <hr />
+        </div>
+      )}
+      <div>
+        <h3>Follow-up discussions</h3>
+        {displayPost.followUpDiscussions.map((discussion) => {
+          console.log(discussion.replies);
+          return (
+            <div key={discussion.id}>
+              <div>
+                <span className="flex flex-row">
+                  <FaUserCircle className="w-10" style={{ fontSize: "26px" }} />
+                  <p className="font-bold">John Doe</p>
+                </span>
+                <p>{discussion.content}</p>
+              </div>
+              {discussion.replies.map((reply) => {
+                return <p key={reply.id}>{reply.content}</p>;
+              })}
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <h5>Start a follow-up discussion</h5>
+        <ReactQuill theme="snow" value={value} onChange={setValue} />
+      </div>
     </div>
   );
 }
